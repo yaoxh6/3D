b627ec80421d6ab7cf152b21e199129d2577a214
yaoxh6 <2879813862@qq.com>
2018-04-09 08:28:08 +0800

    end

---
+++ homework3/Assets/Script/Action/CCMoveToAction.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public class CCMoveToAction : SSAction
+    {
+        public Vector3 target;
+        public float speed;
+
+        private CCMoveToAction() { }
+        public static CCMoveToAction getAction(Vector3 target, float speed)
+        {
+            CCMoveToAction action = ScriptableObject.CreateInstance<CCMoveToAction>();
+            action.target = target;
+            action.speed = speed;
+            return action;
+        }
+
+        public override void Update()
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, target, speed * Time.deltaTime);
+            if (this.transform.position == target)
+            {
+                this.destroy = true;
+                this.callback.actionDone(this);
+            }
+        }
+
+        public override void Start()
+        {
+            //
+        }
+
+    }
---
+++ homework3/Assets/Script/Action/CCSequenceAction.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public class CCSequenceAction : SSAction, ISSActionCallback
+    {
+        public List<SSAction> sequence;
+        public int repeat = 1;
+        public int currentActionIndex = 0;
+
+        public static CCSequenceAction getAction(int repeat, int currentActionIndex, List<SSAction> sequence)
+        {
+            CCSequenceAction action = ScriptableObject.CreateInstance<CCSequenceAction>();
+            action.sequence = sequence;
+            action.repeat = repeat;
+            action.currentActionIndex = currentActionIndex;
+            return action;
+        }
+
+        public override void Update()
+        {
+            if (sequence.Count == 0) return;
+            if (currentActionIndex < sequence.Count)
+            {
+                sequence[currentActionIndex].Update();
+            }
+        }
+
+        public void actionDone(SSAction source)
+        {
+            source.destroy = false;
+            this.currentActionIndex++;
+            if (this.currentActionIndex >= sequence.Count)
+            {
+                this.currentActionIndex = 0;
+                if (repeat > 0) repeat--;
+                if (repeat == 0)
+                {
+                    this.destroy = true;
+                    this.callback.actionDone(this);
+                }
+            }
+        }
+
+        public override void Start()
+        {
+            foreach (SSAction action in sequence)
+            {
+                action.gameObject = this.gameObject;
+                action.transform = this.transform;
+                action.callback = this;
+                action.Start();
+            }
+        }
+
+        void OnDestroy()
+        {
+            foreach (SSAction action in sequence)
+            {
+                DestroyObject(action);
+            }
+        }
+    }
